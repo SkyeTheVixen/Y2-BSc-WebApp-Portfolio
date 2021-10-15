2a7d11360977eb85859b6a67f4a46aa1ff722df0
Skylar <Skylar.beacham@outlook.com>
2021-10-15 12:20:22 +0100

    Update coursemanagement.php

--- coursemanagement.php
+++ coursemanagement.php
@@ -50,6 +50,9 @@
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
         <script>
             $(document).ready(function () {
+            $("#addCourseBtn").click(function (event) {
+                $("#addCourseForm").submit();
+            });
             $("#addCourseForm").submit(function (event) {
                 var data = $(this).serialize();
                 event.preventDefault();
