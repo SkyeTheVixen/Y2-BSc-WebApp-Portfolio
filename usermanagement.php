da5a709e561e9892ff69a8aadad6e8b4eef50dea
Skylar <Skylar.beacham@outlook.com>
2021-10-16 13:55:41 +0100

    Update usermanagement.php

--- usermanagement.php
+++ usermanagement.php
@@ -40,7 +40,7 @@
     <!-- End Metadata and Icons -->
 
     <!-- Stylesheets -->
-    <link rel="stylesheet" href="res/css/usermgt.css">
+    <link rel="stylesheet" href="res/css/global.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <!-- End Stylesheets -->
