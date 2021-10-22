622315d3202f0a98b30fa0be165a33c2594c266a
Skylar <Skylar.beacham@outlook.com>
2021-10-22 10:08:58 +0100

    Update login.php

--- login.php
+++ login.php
@@ -63,8 +63,8 @@
 <body>
 
     <div class="container h-100">
-        <div class="row h-100 align-items-center">
-            <div class="col-auto deflex justify-content-center align-content-center loginform align-items-center shadow">
+        <div class="row h-100 deflex justify-content-center align-content-center">
+            <div class="col-auto loginform align-items-center shadow">
                 <div class="row pt-4">
                     <h1 class="text-center w-100">Login to VD Training</h1>
                 </div>
