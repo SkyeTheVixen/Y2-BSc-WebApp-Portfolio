029217a52bca80b1a63200f90a7d3fe1f55df7f7
Skylar <Skylar.beacham@outlook.com>
2021-10-22 09:53:26 +0100

    Update coursemanagement.php

--- coursemanagement.php
+++ coursemanagement.php
@@ -48,7 +48,8 @@
     <!-- Important scripts -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
 
     <script>
         $(document).ready(function () {
