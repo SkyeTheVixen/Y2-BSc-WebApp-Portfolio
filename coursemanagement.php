d4d4c92144da6129026680792987eba6b83425e6
Skylar <Skylar.beacham@outlook.com>
2021-10-25 17:00:38 +0100

    delete course SWAL

--- coursemanagement.php
+++ coursemanagement.php
@@ -89,6 +89,46 @@
 
             });
 
+            function delCourse (cuid) {
+                $.ajax({
+                    type: "post",
+                    url: "php/delcourse.php",
+                    data: {
+                        cuid: cuid
+                    },
+                    cache: false,
+                    success: function (result) {
+                        var Data = JSON.parse(result);
+                        if (Data.statuscode === 200) {
+                            $("#delUserModal").modal('toggle');
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                        } else if (Data.statuscode === 201) {
+                            alert("Error while deleting User. Try again");
+                        }
+                    }
+                });
+            };
+
+            $(".delCUID").click(function (event) {
+                Swal.fire({
+                    title: 'Are you sure?',
+                    text: "You won't be able to revert this!",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes, delete it!'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        delCourse($(this).attr('data-id'));
+                    }
+                })
+            });
+
             $("#courseTable").DataTable();
         })
     </script>
@@ -152,122 +192,125 @@
         <!-- Table of courses -->
         <!-- Table of Users -->
         <div class="row mt-5">
-        <table class="mt-5 table table-striped table-hover" id="courseTable">
-            <thead>
-                <tr>
-                    <th>Course ID</th>
-                    <th>Course Name</th>
-                    <th>Description</th>
-                    <th>Start Date</th>
-                    <th>End Date</th>
-                    <th>Delivery Method</th>
-                    <th>Allow Self-enrol?</th>
-                    <th>Max Participants</th>
-                    <th>View</th>
-                    <th>Edit</th>
-                    <th>Delete</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
+            <table class="mt-5 table table-striped table-hover" id="courseTable">
+                <thead>
+                    <tr>
+                        <th>Course ID</th>
+                        <th>Course Name</th>
+                        <th>Description</th>
+                        <th>Start Date</th>
+                        <th>End Date</th>
+                        <th>Delivery Method</th>
+                        <th>Allow Self-enrol?</th>
+                        <th>Max Participants</th>
+                        <th>View</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
                         $sql = "SELECT * FROM `tblCourses` ORDER BY `tblCourses`.`CreatedAt` DESC";
                         $run = mysqli_query($connect, $sql);
 
                         while($result = mysqli_fetch_assoc($run))
                         {
                     ?>
-                <tr>
-                    <td><?=$result["CUID"]?></td>
-                    <td><?=$result["CourseTitle"]?></td>
-                    <td><?=$result["CourseDescription"]?></td>
-                    <td><?=$result["StartDate"]?></td>
-                    <td><?=$result["EndDate"]?></td>
-                    <td><?=$result["DeliveryMethod"]?></td>
-                    <td><?=$result["SelfEnrol"]?></td>
-                    <td><?=$result["MaxParticipants"]?></td>
-                    <td><a data-id="<?=$result["CUID"]?>" class="viewCourse"><i class="fa fa-eye"></i></a></td>
-                    <td><a data-id="<?=$result["CUID"]?>" class="editCourse"><i class="fa fa-pencil"></i></a></td>
-                    <td><a data-id="<?=$result["CUID"]?>" class="delCourse" data-bs-toggle="modal"
-                            data-bs-target="#delCourseModal"><i class="fas fa-trash-alt"></i></a>
-                    </td>
-                    <?php
+                    <tr>
+                        <td><?=$result["CUID"]?></td>
+                        <td><?=$result["CourseTitle"]?></td>
+                        <td><?=$result["CourseDescription"]?></td>
+                        <td><?=$result["StartDate"]?></td>
+                        <td><?=$result["EndDate"]?></td>
+                        <td><?=$result["DeliveryMethod"]?></td>
+                        <td><?=$result["SelfEnrol"]?></td>
+                        <td><?=$result["MaxParticipants"]?></td>
+                        <td><a data-id="<?=$result["CUID"]?>" class="viewCourse"><i class="fa fa-eye"></i></a></td>
+                        <td><a data-id="<?=$result["CUID"]?>" class="editCourse"><i class="fa fa-pencil"></i></a></td>
+                        <td><a data-id="<?=$result["CUID"]?>" class="delCourse" data-bs-toggle="modal"
+                                data-bs-target="#delCourseModal"><i class="fas fa-trash-alt"></i></a>
+                        </td>
+                        <?php
                             }
                         ?>
-                </tr>
-            </tbody>
-        </table>
-        <!-- End Table of courses -->
+                    </tr>
+                </tbody>
+            </table>
+            <!-- End Table of courses -->
 
 
-        <button class="px-3 btn btn-primary" data-bs-toggle="modal" data-bs-target="#addCourseModal">Add Course</button>
+            <button class="px-3 btn btn-primary" data-bs-toggle="modal" data-bs-target="#addCourseModal">Add
+                Course</button>
 
 
-        <!-- Add Course Modal -->
-        <div class="modal fade" id="addCourseModal" tabindex="-1" aria-labelledby="addCourseModalLabel"
-            aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-                <div class="modal-content">
-                    <form autocomplete="off" id="addCourseForm">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="addCourseModalLabel">Add New Course</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <div class="mb-3">
-                                <label for="courseNameInput" class="form-label">Course Name</label>
-                                <input type="text" required class="form-control" name="courseNameInput">
+            <!-- Add Course Modal -->
+            <div class="modal fade" id="addCourseModal" tabindex="-1" aria-labelledby="addCourseModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                    <div class="modal-content">
+                        <form autocomplete="off" id="addCourseForm">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="addCourseModalLabel">Add New Course</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
                             </div>
-                            <div class="mb-3">
-                                <label for="courseDescriptionInput" class="form-label">Course Description</label>
-                                <textarea required class="form-control" name="courseDescriptionInput"></textarea>
+                            <div class="modal-body">
+                                <div class="mb-3">
+                                    <label for="courseNameInput" class="form-label">Course Name</label>
+                                    <input type="text" required class="form-control" name="courseNameInput">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="courseDescriptionInput" class="form-label">Course Description</label>
+                                    <textarea required class="form-control" name="courseDescriptionInput"></textarea>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="courseStartDateInput" class="form-label">Start Date</label>
+                                    <input type="date" required class="form-control" name="courseStartDateInput">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="courseEndDateInput" class="form-label">End Date</label>
+                                    <input type="date" required class="form-control" name="courseEndDateInput">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="courseDeliveryMethod" class="form-label">Delivery Method</label>
+                                    <select class="form-select" required name="courseDeliveryMethod"
+                                        aria-label="selectAccessLevel">
+                                        <option selected>Delivery Method</option>
+                                        <option value="In Person">In Person</option>
+                                        <option value="Online">Online</option>
+                                    </select>
+                                </div>
+                                <div class="mb-3 form-check form-switch">
+                                    <label for="courseSelfEnrol" class="form-label">Allow Self Enrolment?</label>
+                                    <input class="form-check-input" type="checkbox" role="switch"
+                                        name="courseSelfEnrol">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="courseMaxParticipants" class="form-label">Maximum Participants</label>
+                                    <input type="number" required class="form-control" name="courseMaxParticipants">
+                                </div>
                             </div>
-                            <div class="mb-3">
-                                <label for="courseStartDateInput" class="form-label">Start Date</label>
-                                <input type="date" required class="form-control" name="courseStartDateInput">
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                                <button type="submit" class="btn btn-primary" id="addCourseBtn">Add Course</button>
                             </div>
-                            <div class="mb-3">
-                                <label for="courseEndDateInput" class="form-label">End Date</label>
-                                <input type="date" required class="form-control" name="courseEndDateInput">
-                            </div>
-                            <div class="mb-3">
-                                <label for="courseDeliveryMethod" class="form-label">Delivery Method</label>
-                                <select class="form-select" required name="courseDeliveryMethod"
-                                    aria-label="selectAccessLevel">
-                                    <option selected>Delivery Method</option>
-                                    <option value="In Person">In Person</option>
-                                    <option value="Online">Online</option>
-                                </select>
-                            </div>
-                            <div class="mb-3 form-check form-switch">
-                                <label for="courseSelfEnrol" class="form-label">Allow Self Enrolment?</label>
-                                <input class="form-check-input" type="checkbox" role="switch" name="courseSelfEnrol">
-                            </div>
-                            <div class="mb-3">
-                                <label for="courseMaxParticipants" class="form-label">Maximum Participants</label>
-                                <input type="number" required class="form-control" name="courseMaxParticipants">
-                            </div>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                            <button type="submit" class="btn btn-primary" id="addCourseBtn">Add Course</button>
-                        </div>
-                    </form>
+                        </form>
 
+                    </div>
                 </div>
             </div>
+            <!-- End Add Course Modal -->
+
+
         </div>
-        <!-- End Add Course Modal -->
+        <!-- End Main Page Content -->
 
-
-    </div>
-    <!-- End Main Page Content -->
-
-    <!-- Scripts -->
-    <script src="https://kit.fontawesome.com/93e867abff.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous">
-    </script>
-    <!-- End Scripts -->
+        <!-- Scripts -->
+        <script src="https://kit.fontawesome.com/93e867abff.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous">
+        </script>
+        <!-- End Scripts -->
 </body>
 
 </html>
