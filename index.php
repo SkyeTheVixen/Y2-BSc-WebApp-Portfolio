d8d194d0b82d1bbfb2128ec4732c43a34705d8e1
Skylar <Skylar.beacham@outlook.com>
2021-11-04 21:19:19 +0000

    removed property on nav

--- index.php
+++ index.php
@@ -81,7 +81,7 @@
                 aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="collapse navbar-collapse w-100" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
                         <a class="nav-link link-light active" aria-current="page" href="index"><i
