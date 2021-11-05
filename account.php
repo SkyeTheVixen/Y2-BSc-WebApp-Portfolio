ba2eeed9e4b45bf03c424fc837f5eb112fa34650
Skylar <Skylar.beacham@outlook.com>
2021-11-05 10:03:24 +0000

    Move func to external file

--- account.php
+++ account.php
@@ -35,19 +35,8 @@
     <!-- Important scripts -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <?php
-        $sql = "SELECT * FROM `tblUsers` WHERE `tblUsers`.`UUID` = ?";
-        $stmt = mysqli_prepare($connect, $sql);
-        mysqli_stmt_bind_param($stmt, 's', $_SESSION["userID"]);
-        $stmt -> execute();
-        $result = $stmt->get_result();
-        if($result -> num_rows === 1){
-            $User = $result->fetch_array(MYSQLI_ASSOC);
-            if($User["AccessLevel"] === "user"){
-                echo "<script type='text/javascript'>$(document).ready(function () { $('#mgtDrop').remove(); })</script>";
-            }
-        }
-    ?>
+    <?php include("./php/removedropdown.php"); ?>
+
     <!-- End Important scripts -->
 </head>
 
--- index.php
+++ index.php
@@ -49,19 +49,7 @@
             })
         })
     </script>
-    <?php
-        $sql = "SELECT * FROM `tblUsers` WHERE `tblUsers`.`UUID` = ?";
-        $stmt = mysqli_prepare($connect, $sql);
-        mysqli_stmt_bind_param($stmt, 's', $_SESSION["userID"]);
-        $stmt -> execute();
-        $result = $stmt->get_result();
-        if($result -> num_rows === 1){
-            $User = $result->fetch_array(MYSQLI_ASSOC);
-            if($User["AccessLevel"] === "user"){
-                echo "<script type='text/javascript'>$(document).ready(function () { $('#mgtDrop').remove(); })</script>";
-            }
-        }
-    ?>
+    <?php include("./php/removedropdown.php"); ?>
     <!-- End Important Scripts -->
 
 
