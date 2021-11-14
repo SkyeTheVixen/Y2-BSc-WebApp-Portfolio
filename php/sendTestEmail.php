c29308beb9e4389ade8811abc4ecc6addae9d640
Skylar <Skylar.beacham@outlook.com>
2021-11-14 23:45:20 +0000

    modify test script

--- php/sendTestEmail.php
+++ php/sendTestEmail.php
@@ -1,13 +1,42 @@
 <?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+    require 'vendor/autoload.php';
 
-include("functions.inc.php");
-
-$to = "skylar.beacham@outlook.com";
+    $to = "skylar.beacham@outlook.com";
 $subject = "Test Email";
 $userName = "Skylar Beacham";
 $txt = "Test Email";
-sendMail($to, $userName,  $subject, $txt, $txt);
 
+
+    $mail = new PHPMailer(true);
+    try {
+        //Server settings
+        $mail->isSMTP();
+        $mail->Host       = 'mail.vixendev.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'no-reply@vixendev.com';
+        $mail->Password   = 'Orange@72';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+
+        //Recipients
+        $mail->setFrom('no-reply@vixendev.com', 'Vixendev');
+        $mail->addAddress($to, $userName);
+
+        //Content
+        $mail->isHTML(true);
+        $mail->Subject = $subject;
+        $mail->Body    = $txt;
+        $mail->AltBody = $txt;
+
+        $mail->send();
+        echo 'Message has been sent';
 echo json_encode(array("statuscode" => 200));
+
+    } catch (Exception $e) {
+        file_put_contents("errorlog.txt", "Message could not be sent. Mailer Error: {$mail->ErrorInfo}");
+    }
+
 
 ?>
