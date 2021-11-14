5572338f5a7095307d753706b6d94642fce0824c
Skylar <Skylar.beacham@outlook.com>
2021-11-14 13:06:25 +0000

    Moved fontawesome to head

--- php/_footer.php
+++ php/_footer.php
@@ -1,4 +1,3 @@
     <!-- Scripts -->
-    <script src="https://kit.fontawesome.com/93e867abff.js" crossorigin="anonymous"></script>
-    </script>
+
     <!-- End Scripts -->
--- php/_header.php
+++ php/_header.php
@@ -30,6 +30,8 @@
     <script src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap5.min.js"></script>
     <script type="text/javascript" src='../res/js/<?php echo($currentPage); ?>.js'></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/93e867abff.js" crossorigin="anonymous"></script>
+    </script>
     <!-- End Important Scripts -->
     <?php include("./removedropdown.php"); ?>
 
