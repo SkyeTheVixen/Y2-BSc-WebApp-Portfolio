e0e7e6a740769cf721ba7c5552f708d76a315a95
Skylar <Skylar.beacham@outlook.com>
2021-11-14 23:41:54 +0000

    modified test email

--- php/sendTestEmail.php
+++ php/sendTestEmail.php
@@ -1,9 +1,9 @@
 <?php
 
-$to = $email;
-$subject = "User Account Creation";
-$userName = $firstName . " " . $lastName;
-$txt = "Hi ".$firstName." ".$lastName.".\n\nA User account has been created for you on the training platform. The login details are listed below.\n\n\nUsername: ".$email."\nPassword: ".$password."\nURL: https://ws255237-wad.remote.ac\n\nKind Regards,\nVD Training Team\n\n";
+$to = "skylar.beacham@outlook.com";
+$subject = "Test Email";
+$userName = "Skylar Beacham";
+$txt = "Test Email";
 sendMail($to, $userName,  $subject, $txt, $txt);
 
 echo json_encode(array("statuscode" => 200));
