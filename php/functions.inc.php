80f001819fb100c9a9bb571ae7cee02d38cbcc3e
Skylar <Skylar.beacham@outlook.com>
2021-11-15 00:19:07 +0000

    maybe fixed file path for vendor?

--- php/functions.inc.php
+++ php/functions.inc.php
@@ -2,7 +2,7 @@
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\SMTP;
     use PHPMailer\PHPMailer\Exception;
-    require '../vendor/autoload.php';
+    require '../../vendor/autoload.php';
 
     function GenerateID() {
         $IDData = $IDData ?? random_bytes(16);
