3c46aadc55f6fe94f07c1898c1cf5410e9fde196
Skylar <Skylar.beacham@outlook.com>
2021-11-15 00:28:04 +0000

    modify to prove point

--- php/sendTestEmail.php
+++ php/sendTestEmail.php
@@ -1,39 +1,39 @@
 <?php
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\SMTP;
-    use PHPMailer\PHPMailer\Exception;
-    require '../vendor/autoload.php';
+    // use PHPMailer\PHPMailer\PHPMailer;
+    // use PHPMailer\PHPMailer\SMTP;
+    // use PHPMailer\PHPMailer\Exception;
+    // require '../vendor/autoload.php';
 
 
-    $mail = new PHPMailer(true);
-    try {
-        //Server settings
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-        $mail->isSMTP();
-        $mail->Host       = 'mail.vixendev.com';
-        $mail->SMTPAuth   = true;
-        $mail->Username   = 'no-reply@vixendev.com';
-        $mail->Password   = 'Orange@72';
-        $mail->Port       = 465;
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    // $mail = new PHPMailer(true);
+    // try {
+    //     //Server settings
+    //     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    //     $mail->isSMTP();
+    //     $mail->Host       = 'mail.vixendev.com';
+    //     $mail->SMTPAuth   = true;
+    //     $mail->Username   = 'no-reply@vixendev.com';
+    //     $mail->Password   = 'Orange@72';
+    //     $mail->Port       = 465;
+    //     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
 
-        //Recipients
-        $mail->setFrom('no-reply@vixendev.com', 'Vixendev');
-        $mail->addAddress("warningatom9922@gmail.com", "Skylar");
+    //     //Recipients
+    //     $mail->setFrom('no-reply@vixendev.com', 'Vixendev');
+    //     $mail->addAddress("warningatom9922@gmail.com", "Skylar");
 
-        //Content
-        $mail->isHTML(true);
-        $mail->Subject = "Test Email";
-        $mail->Body    = "Config correct";
-        $mail->AltBody = "Config correct";
+    //     //Content
+    //     $mail->isHTML(true);
+    //     $mail->Subject = "Test Email";
+    //     $mail->Body    = "Config correct";
+    //     $mail->AltBody = "Config correct";
 
-        $mail->send();
-        echo json_encode(array("statuscode" => 200));
+    //     $mail->send();
+    //     echo json_encode(array("statuscode" => 200));
 
-    } catch (Exception $e) {
-        echo json_encode(array("statuscode" => 201));
-        file_put_contents("errorlog.txt", "Message could not be sent. Mailer Error: {$mail->ErrorInfo}");
-    }
+    // } catch (Exception $e) {
+    //     echo json_encode(array("statuscode" => 201));
+    //     file_put_contents("errorlog.txt", "Message could not be sent. Mailer Error: {$mail->ErrorInfo}");
+    // }
 
-
+        mail("warningatom9922@gmail.com", "Test", "test", 'From: no-reply@vixendev.com');
 ?>
