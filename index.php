bcec349c50cddee0336fd8a81e0fcd43da2ae595
Skylar <Skylar.beacham@outlook.com>
2022-01-11 10:58:40 +0000

    Created Dummy page

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    $currentPage="index";
+    $title="Home | VD Training";
+    include("res/php/_connect.php");
+    include("res/php/_authcheck.php");
+    include("res/php/header.php"); 
+    include("res/php/navbar.php");
+?>
+
+<!-- Main Content -->
+
+<!-- End Main Content -->
+
+<?php
+    include("res/php/footer.php");
+?>
