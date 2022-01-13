de190d538bf2339cdb51ed49dc2eb0066a4fd79a
Skylar <Skylar.beacham@outlook.com>
2022-01-13 18:20:05 +0000

    Add account page

---
+++ account.php
@@ -0,0 +1,32 @@
+<?php
+    $currentPage="account";
+    $title="Account | VD Training";
+    include("res/php/_connect.php");
+    // include("res/php/_authcheck.php");
+    include("res/php/header.php"); 
+    include("res/php/navbar.php");
+    $user=getLoggedInUser($mysqli);
+?>
+
+
+<!-- Main Content -->
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <div class="row h-25">
+                <div class="col-8">
+                    <h1><?=htmlspecialchars($user["FirstName"]." ".$user["LastName"]); ?></h1>
+                </div>
+                <div class="col-4">
+                        <img src="https://proficon.stablenetwork.uk/api/identicon/<?=$user["UserID"];?>.svg" alt="Profile Icon" class="h-50">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Main Content -->
+
+
+<?php
+    include("res/php/footer.php");
+?>
