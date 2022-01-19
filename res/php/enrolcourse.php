23109c9030b462e6dae234890309e0d39c993075
Skylar <Skylar.beacham@outlook.com>
2022-01-19 22:23:54 +0000

    Add log in check

--- res/php/enrolcourse.php
+++ res/php/enrolcourse.php
@@ -1,5 +1,29 @@
 <?php
+    //If user aint logged in, redirect them
+    session_start();
+    if (!isset($_SESSION['UserID'])){
+        header("Location: ../../login");
+    }
 
-    echo json_encode(array("statuscode"=>200));
+    //Include the connection and functions
+    include_once("_connect.php");
+    include_once("functions.inc.php");
+
+    $mysqli->autocommit(FALSE);
+    //Escape the course ID, just in case
+    $course_id = $mysqli->real_escape_string($_POST['course_id']);
+    $sql = "INSERT INTO `tblUserCourses` (`UUID`, `CUID`) VALUES ('?', '?');";
+    $stmt = $mysqli->prepare($sql);
+    $stmt->bind_param("ss", $_SESSION['UUID'], $course_id);
+    if($stmt->execute()){
+        $mysqli->commit();
+        echo json_encode(array("statuscode"=>200)); //Only one return as no one could possibly submit bad data
+
+    }
+    else{
+        $mysqli->rollback();
+        echo json_encode(array("statuscode"=>201)); //Added a return as some one could possibly submit bad data
+    }
+    $stmt->close();
 
 ?>
