e5f68d89886d6827bc49a5b3ba9c1df765fccfaf
Skylar <Skylar.beacham@outlook.com>
2022-01-19 23:06:00 +0000

    Condensed autoloader

--- res/php/autoload.php
+++ res/php/autoload.php
@@ -1,16 +1,7 @@
 <?php
 
     //Get the autoload composer file from wherever it is located
-    // if(file_exists("../vendor/autoload.php")){
-    //     require '../vendor/autoload.php';
-    // }else if(file_exists("vendor/autoload.php")){
-    //     require 'vendor/autoload.php';
-    // }else if(file_exists("./vendor/autoload.php")){
-    //     require './vendor/autoload.php';
-    // }else if(file_exists("../../vendor/autoload.php")){
-    //     require '../../vendor/autoload.php';
-    // }
     require __DIR__ . '../../../vendor/autoload.php';
-
     return Dotenv\Dotenv::createImmutable(__DIR__);
+    
 ?>
