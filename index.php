57c126f2af821632b2b8273034779ea7fc449b00
Skylar <Skylar.beacham@outlook.com>
2022-01-19 14:08:38 +0000

    Remove authcheck temporarily

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     $currentPage="index";
     $title="Home | VD Training";
     include("res/php/_connect.php");
-    include("res/php/_authcheck.php");
+    // include("res/php/_authcheck.php");
     include("res/php/header.php"); 
     include("res/php/navbar.php");
 ?>
