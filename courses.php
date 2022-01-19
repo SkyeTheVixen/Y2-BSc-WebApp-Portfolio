05366539cd5be79eb50e787f369bfc64d63c58f8
Skylar <Skylar.beacham@outlook.com>
2022-01-19 22:27:23 +0000

    add function script

--- courses.php
+++ courses.php
@@ -5,6 +5,7 @@
     $pageRedirect="";
     include("res/php/_connect.php");
     include("res/php/_authcheck.php");
+    include("res/php/functions.inc.php");
     include("res/php/header.php"); 
     include("res/php/navbar.php");
     $mysqli->autocommit(false);
