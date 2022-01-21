0393eb6b76deab3d8f52e06f54c004778c80bc05
Skylar <Skylar.beacham@outlook.com>
2022-01-21 20:41:06 +0000

    Fix User add missing

--- res/js/usermanagement.js
+++ res/js/usermanagement.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
 
         $.ajax({
             type: "post",
-            url: "../../php/adduser.php",
+            url: "res/php/adduser.php",
             data: {
                 email: email,
                 password: password,
