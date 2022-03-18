e0377e92c438a82a76a126868721aa5c84084488
Skylar Beacham <skylar@vixendev.com>
2022-03-18 11:10:00 +0000

    Move jQuery to header

--- res/php/footer.php
+++ res/php/footer.php
@@ -20,7 +20,6 @@
         <p class="text-center text-muted pb-3">© 2021 - <?= date("Y");?> Vixendev</p>
     </footer>
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="  crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
     <script type="text/javascript" src="<?=$pathHead?>js/<?=$currentPage;?>.js" ></script>
     <script src="https://kit.fontawesome.com/93e867abff.js" crossorigin="anonymous" ></script>
--- res/php/header.php
+++ res/php/header.php
@@ -34,5 +34,6 @@
     <link href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css" rel="stylesheet">
     <!-- End Stylesheets -->
 
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="  crossorigin="anonymous"></script>
     <?=removeDropdown($mysqli, $currentPage); ?>
 </head>
