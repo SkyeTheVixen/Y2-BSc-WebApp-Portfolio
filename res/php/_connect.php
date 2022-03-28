daaa37008ebf93960efdb77684a359358373eb70
Skylar <Skylar.beacham@outlook.com>
2022-03-28 23:18:22 +0100

    autocommit off

--- res/php/_connect.php
+++ res/php/_connect.php
@@ -7,6 +7,7 @@
     
     //Connect to Maria Server
     $mysqli = mysqli_connect($_ENV["DB_HOST"], $_ENV["DB_USER"], $_ENV["DB_PASS"], $_ENV["DB_NAME"]);
+    $mysqli->autocommit(false);
     if(!$mysqli) {
         die("Connection failed: " . mysqli_connect_error());
     }
