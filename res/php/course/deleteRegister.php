7456000bdf7398dd77d02a7848d7aaf9b894546c
Skylar <Skylar.beacham@outlook.com>
2022-03-29 18:44:43 +0100

    Update path

--- res/php/course/deleteRegister.php
+++ res/php/course/deleteRegister.php
@@ -3,6 +3,7 @@
     if (!isset($_SESSION['UserID'])){
         header("Location: ../login");
     }
+    
     //This file will delete the register when complete
     unlink('../../'.$_POST['URL']);
     return;
