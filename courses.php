d97171f90241b8b4807ffdae4f7baa7f5580e79a
Skylar Beacham <skylar@vixendev.com>
2022-04-01 12:16:03 +0100

    aaaaaa

--- courses.php
+++ courses.php
@@ -26,12 +26,12 @@
     <!-- Tab list -->
     <ul class="nav nav-tabs" id="TabbedCourses" role="tablist">
         <li class="nav-item" role="presentation">
-            <button class="nav-link active" id="upcoming-tab" data-bs-toggle="tab" data-bs-target="#upcoming" type="button"
-                role="tab" aria-controls="upcoming" aria-selected="true">Upcoming</button>
+            <button class="nav-link active" id="upcoming-tab" data-bs-toggle="tab" data-bs-target="#upcoming"
+                type="button" role="tab" aria-controls="upcoming" aria-selected="true">Upcoming</button>
         </li>
         <li class="nav-item" role="presentation">
-            <button class="nav-link" id="past-tab" data-bs-toggle="tab" data-bs-target="#past" type="button"
-                role="tab" aria-controls="past" aria-selected="false">Past</button>
+            <button class="nav-link" id="past-tab" data-bs-toggle="tab" data-bs-target="#past" type="button" role="tab"
+                aria-controls="past" aria-selected="false">Past</button>
         </li>
     </ul>
     <!-- End Tab List -->
@@ -64,29 +64,38 @@
                                 <li class="list-group-item">Start Date: <?= $rows['StartDate']; ?></li>
                                 <li class="list-group-item">End Date: <?=$rows['EndDate']; ?></li>
                                 <li class="list-group-item">Delivery Method: <?=$rows['DeliveryMethod']; ?></li>
-                                <li class="list-group-item" id="participantCount-<?=$rows["CUID"];?>">Participants: <?=$rows['CurrentParticipants']?>/<?=$rows['MaxParticipants']; ?></li>
-                                <li class="list-group-item"><progress id="progress-<?=$rows["CUID"];?>" max="<?=$rows['MaxParticipants']; ?>" value="<?=$rows['CurrentParticipants']?>"></progress></li>
+                                <li class="list-group-item" id="participantCount-<?=$rows["CUID"];?>">Participants:
+                                    <?=$rows['CurrentParticipants']?>/<?=$rows['MaxParticipants']; ?></li>
+                                <li class="list-group-item"><progress id="progress-<?=$rows["CUID"];?>"
+                                        max="<?=$rows['MaxParticipants']; ?>"
+                                        value="<?=$rows['CurrentParticipants']?>"></progress></li>
                                 <li class="list-group-item d-flex" id="btns<?=$rows["CUID"];?>">
                                     <?php if((($rows['CurrentParticipants'] < $rows['MaxParticipants']) && $rows['SelfEnrol'] == 1) || (($rows['CurrentParticipants'] < $rows['MaxParticipants']) && isAdmin($mysqli))){?>
-                                        <?php if(UserIsEnrolled($mysqli, $rows['CUID'])){ ?>
-                                                <a class="btn btn-success disabled" disabled>✅ Enrolled!</a>
-                                            <a data-courseid="<?=$rows['CUID']; ?>" class="btn btn-danger btn-unenrol">Unenroll</a></li>
-                                        <?php } else { ?>
-                                            <a data-courseid="<?=$rows['CUID']; ?>" class="btn btn-enrol btn-primary">Register</a>
-                                        <?php } ?>
-                                    <?php } else { ?>                                    
-                                        <?php if(UserIsEnrolled($mysqli, $rows['CUID'])){ ?>
-                                            <?php if($rows["SelfEnrol"] == 0){?>
-                                                <a data-courseid="<?=$rows['CUID']; ?>"  class="btn btn-enrol btn-success disabled me-1" disabled>✅ Enrolled!</a>
-                                                <a data-courseid="<?=$rows['CUID']; ?>" class="btn btn-unenrol btn-danger disabled" disabled>Unenroll</a>
-                                                    <?php } ?>
-                                                <a data-courseid="<?=$rows['CUID']; ?>" class="btn btn-unenrol btn-danger">Unenroll</a>
-                                                <a data-courseid="<?=$rows['CUID']; ?>"  class="btn btn-enrol btn-success disabled me-1" disabled>✅ Enrolled!</a>
-
-                                        <?php } else { ?>
-                                            <a data-courseid="<?=$rows['CUID']; ?>" class="enrol-btn btn btn-secondary disabled" disabled title="Please speak to your admin to request access to this course">Register</a>
-                                        <?php } ?>
-                                    <?php } ?>
+                                    <?php if(UserIsEnrolled($mysqli, $rows['CUID'])){ ?>
+                                    <a class="btn btn-success disabled" disabled>✅ Enrolled!</a>
+                                    <a data-courseid="<?=$rows['CUID']; ?>"
+                                        class="btn btn-danger btn-unenrol">Unenroll</a></li>
+                                <?php } else { ?>
+                                <a data-courseid="<?=$rows['CUID']; ?>" class="btn btn-enrol btn-primary">Register</a>
+                                <?php } ?>
+                                <?php } else { ?>
+                                <?php if(UserIsEnrolled($mysqli, $rows['CUID'])){ ?>
+                                <?php if($rows["SelfEnrol"] == 0){?>
+                                <a data-courseid="<?=$rows['CUID']; ?>" class="btn btn-enrol btn-success disabled me-1"
+                                    disabled>✅ Enrolled!</a>
+                                <a data-courseid="<?=$rows['CUID']; ?>" class="btn btn-unenrol btn-danger disabled"
+                                    disabled>Unenroll</a>
+                                <?php } else { ?>
+                                <a data-courseid="<?=$rows['CUID']; ?>" class="btn btn-unenrol btn-danger">Unenroll</a>
+                                <a data-courseid="<?=$rows['CUID']; ?>" class="btn btn-enrol btn-success disabled me-1"
+                                    disabled>✅ Enrolled!</a>
+                                <?php } ?>
+                                <?php } else { ?>
+                                <a data-courseid="<?=$rows['CUID']; ?>" class="enrol-btn btn btn-secondary disabled"
+                                    disabled
+                                    title="Please speak to your admin to request access to this course">Register</a>
+                                <?php } ?>
+                                <?php } ?>
 
                                 </li>
                             </ul>
@@ -101,7 +110,7 @@
 
         <!-- Past Courses -->
         <div class="tab-pane fade" id="past" role="tabpanel" aria-labelledby="past-tab">
-        <?php
+            <?php
                 $sql="SELECT * FROM `tblCourses` WHERE date(`EndDate`) < cast(now() as date) ORDER BY `StartDate` ASC";
                 $stmt = $mysqli->prepare($sql);
                 $stmt->execute();
@@ -131,17 +140,22 @@
                                         max="<?=$rows['MaxParticipants']; ?>"
                                         value="<?=$rows['CurrentParticipants']?>"></progress>
                                     <?php if((($rows['CurrentParticipants'] < $rows['MaxParticipants']) && $rows['SelfEnrol'] == 1) || (($rows['CurrentParticipants'] < $rows['MaxParticipants']) && isAdmin($mysqli))){?>
-                                        <?php if(UserIsEnrolled($mysqli, $rows['CUID'])){ ?>
-                                                <a data-courseid="<?=$rows['CUID']; ?>" class="btn btn-enrol btn-success disabled me-1" disabled>✅ Enrolled!</a>
-                                        <?php } else { ?>
-                                            <a data-courseid="<?=$rows['CUID']; ?>" class="btn btn-enrol btn-primary disabled" disabled>Register</a>
-                                        <?php } ?>
-                                    <?php } else { ?>                                    
-                                        <?php if(UserIsEnrolled($mysqli, $rows['CUID'])){ ?>
-                                                <a data-courseid="<?=$rows['CUID']; ?>"  class="btn btn-enrol btn-success disabled me-1" disabled>✅ Enrolled!</a>
-                                        <?php } else { ?>
-                                            <a data-courseid="<?=$rows['CUID']; ?>" class="enrol-btn btn btn-secondary disabled" disabled title="Please speak to your admin to request access to this course">Register</a>
-                                        <?php } ?>
+                                    <?php if(UserIsEnrolled($mysqli, $rows['CUID'])){ ?>
+                                    <a data-courseid="<?=$rows['CUID']; ?>"
+                                        class="btn btn-enrol btn-success disabled me-1" disabled>✅ Enrolled!</a>
+                                    <?php } else { ?>
+                                    <a data-courseid="<?=$rows['CUID']; ?>" class="btn btn-enrol btn-primary disabled"
+                                        disabled>Register</a>
+                                    <?php } ?>
+                                    <?php } else { ?>
+                                    <?php if(UserIsEnrolled($mysqli, $rows['CUID'])){ ?>
+                                    <a data-courseid="<?=$rows['CUID']; ?>"
+                                        class="btn btn-enrol btn-success disabled me-1" disabled>✅ Enrolled!</a>
+                                    <?php } else { ?>
+                                    <a data-courseid="<?=$rows['CUID']; ?>" class="enrol-btn btn btn-secondary disabled"
+                                        disabled
+                                        title="Please speak to your admin to request access to this course">Register</a>
+                                    <?php } ?>
                                     <?php } ?>
                             </ul>
                         </div>
@@ -149,7 +163,7 @@
                 </div>
                 <?php } ?>
             </div>
-            <!-- End Classes Cards --> 
+            <!-- End Classes Cards -->
         </div>
     </div>
 
